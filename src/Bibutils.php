5a406d98584b9db851d8f597d9f82903449edae0
Diego Macrini <macrini@proximify.ca>
2020-11-03 18:23:20 -0500

    Testing the basics

--- src/Bibutils.php
+++ src/Bibutils.php
@@ -6,7 +6,13 @@
  * @license   MIT
  */
 
-namespace Proximify\Bibutils;
+namespace Proximify;
+
+require_once 'vendor/autoload.php';
+
+use Exception;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 /**
  * Template class.
@@ -22,5 +28,62 @@ class Bibutils
      */
     public function __construct(array $options = [])
     {
+    }
+
+    public function parse(string $data, string $format): string
+    {
+        return '';
+    }
+
+    public function encode(string $mods, string $format): string
+    {
+        return '';
+    }
+
+    public function readFile(array $options)
+    {
+        $options['.env'] = null;
+        print_r($options);
+
+        $filename = realpath($options['filename']);
+        $format = $options['format'];
+
+        // string $filename, ?string $format = null
+        $format = $format ?? pathinfo($filename, PATHINFO_EXTENSION);
+
+        $cmd = $format . '2xml';
+
+        $xml = $this->run($cmd, $filename);
+
+        echo $xml;
+
+        return $xml;
+    }
+
+    public function writeFile(string $filename, string $mods, string $format = null)
+    {
+    }
+
+    public function convert(string $srcFilename, string $tgtFilename, ?string $srcFormat = null, ?string $tgtFormat = null): void
+    {
+    }
+
+    protected function run($appName, $filename)
+    {
+        if (!is_file($filename)) {
+            throw new Exception("Cannot find file '$filename'");
+        }
+
+        $cmd = [$appName, $filename];
+
+        $process = new Process($cmd);
+
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return $process->getOutput();
     }
 }
